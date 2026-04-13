8a473cdc162f677d5007143ba92104023b86934e
Alex Soto <asotobu@gmail.com>
2026-04-13 12:24:57 +0200

    Updates auth example

--- ch10/mcp-server-advanced/src/main/java/org/acme/McpClientRest.java
+++ ch10/mcp-server-advanced/src/main/java/org/acme/McpClientRest.java
@@ -5,6 +5,8 @@ import dev.langchain4j.agent.tool.ToolSpecification;
 import dev.langchain4j.mcp.client.McpClient;
 import dev.langchain4j.service.tool.ToolExecutionResult;
 import io.quarkiverse.langchain4j.mcp.runtime.McpClientName;
+import io.quarkus.oidc.client.OidcClient;
+import io.quarkus.oidc.client.runtime.TokensHelper;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -18,8 +20,15 @@ public class McpClientRest {
   @McpClientName("customer")
   McpClient customerClient;
 
+  @Inject
+  OidcClient client;
+  TokensHelper tokenHelper = new TokensHelper();
+
   @GET
   public String getCustomers() {
+
+    System.out.println(client);
+
     List<ToolSpecification> toolSpecifications = customerClient.listTools();
     ToolSpecification customerSpec = toolSpecifications.stream()
         .filter(ts -> "customers".equals(ts.name()))
